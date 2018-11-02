b3e1b88bc12ec7d4c7955074fea20e5818169807
SOF <animeshk55@gmail.com>
2018-11-02 15:06:58 +0530

    bolier plate setupping

--- App.js
+++ App.js
@@ -1,25 +1,48 @@
 import React, {Component} from 'react';
 import { View} from 'react-native';
-// import firebase from 'firebase';
-import firebase from '@firebase/app';
+import firebase from '@firebase/app'
 import '@firebase/auth';
-import '@firebase/database';
+
 import { Header } from './src/components/common';
 import LoginForm from './src/components/LoginForm';
 
 
 class App extends Component {
-  
-  componentWillMount() {
-    firebase.initializeApp({
+
+initializeFirebase() {
+  const firebase = require("firebase");
+
+  // Initialize Firebase
+  var config = {
       apiKey: 'AIzaSyDpC3MY8oU6rveSP6DNVyWAfYLPRJIuajU',
       authDomain: 'authenticationfirenative.firebaseapp.com',
       databaseURL: 'https://authenticationfirenative.firebaseio.com',
       projectId: 'authenticationfirenative',
       storageBucket: 'authenticationfirenative.appspot.com',
       messagingSenderId: '572832231141'
-    });
-    }
+  };
+  firebase.initializeApp(config);
+
+  //inicializando o firestore
+  const firestore = require("firebase/firestore");
+  db = firebase.firestore();
+  db.settings({ timestampsInSnapshots: true });
+}
+
+componentWillMount() {
+  this.initializeFirebase();
+}
+  
+  // componentWillMount() {
+  //   // firebase.initializeApp({
+  //   //   apiKey: 'AIzaSyDpC3MY8oU6rveSP6DNVyWAfYLPRJIuajU',
+  //   //   authDomain: 'authenticationfirenative.firebaseapp.com',
+  //   //   databaseURL: 'https://authenticationfirenative.firebaseio.com',
+  //   //   projectId: 'authenticationfirenative',
+  //   //   storageBucket: 'authenticationfirenative.appspot.com',
+  //   //   messagingSenderId: '572832231141'
+  //   // });
+  //   }
 
   render() {
     return (
