ab60186442a0f40e33e1c5bc928bf6e1dd8af76b
SOF <animeshk55@gmail.com>
2018-11-02 12:33:27 +0530

    error in firebase import module as tried but failed till and now trying again

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,24 @@
 import React, {Component} from 'react';
+import { Text } from 'react-native';
+// import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-    state = { email: '', password: '' };
+    state = { email: '', password: ''};
+    
+    // onButtonPress.auth() {
+    //     const { email, password } = this.state;
+
+    //     this.setState({ error: '' });
+
+    //     firebase.auth().signInWithEmailAndPassword(email, password)
+    //         .catch(() => {
+    //             firebase.auth().createUserWithEmailAndPassword(email, password)
+    //                 .catch(() => {
+    //                     this.setState({ error: 'Authentication Failed' });
+    //                 });
+    //         });
+    // }
     
     render() {
         return (
@@ -25,8 +41,14 @@ class LoginForm extends Component {
                     />
                 </CardSection>
 
+                {/* <Text style={styles.errorTextStyle}>
+                    {this.state.error}
+                </Text> */}
+
                 <CardSection>
-                    <Button>
+                    <Button 
+                    // onPress={this.onButtonPress.bind(this)}
+                    >
                         Login 
                     </Button>
                 </CardSection>
@@ -34,5 +56,13 @@ class LoginForm extends Component {
         );
     }
 }
+
+// const styles = {
+//     errorTextStyle: {
+//         fontSize: 20,
+//         alignSelf: 'center';
+//         color: 'red'
+//     }
+// };
 
 export default LoginForm;
