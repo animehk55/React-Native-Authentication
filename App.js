349655097cb88394a43e0b4a732bd40a04e46bd6
SOF <animeshk55@gmail.com>
2018-11-04 00:48:18 +0530

    Added few modification for firebase error

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import { View} from 'react-native';
+import firebase from '@firebase/app';
+import '@firebase/auth';
 import { Header } from './src/components/common';
 import LoginForm from './src/components/LoginForm';
 
@@ -30,16 +32,16 @@ class App extends Component {
 //   this.initializeFirebase();
 // }
   
-  // componentWillMount() {
-  //   // firebase.initializeApp({
-  //   //   apiKey: 'AIzaSyDpC3MY8oU6rveSP6DNVyWAfYLPRJIuajU',
-  //   //   authDomain: 'authenticationfirenative.firebaseapp.com',
-  //   //   databaseURL: 'https://authenticationfirenative.firebaseio.com',
-  //   //   projectId: 'authenticationfirenative',
-  //   //   storageBucket: 'authenticationfirenative.appspot.com',
-  //   //   messagingSenderId: '572832231141'
-  //   // });
-  //   }
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDpC3MY8oU6rveSP6DNVyWAfYLPRJIuajU',
+      authDomain: 'authenticationfirenative.firebaseapp.com',
+      databaseURL: 'https://authenticationfirenative.firebaseio.com',
+      projectId: 'authenticationfirenative',
+      storageBucket: 'authenticationfirenative.appspot.com',
+      messagingSenderId: '572832231141'
+    });
+    }
 
   render() {
     return (
