7a59450417fade84b3f494ebeeebd1c72ce935bf
SOF <animeshk55@gmail.com>
2018-11-04 11:59:53 +0530

    added spinner ommon component but unable to put it in right way as if clicked button the App closes

--- App.js
+++ App.js
@@ -7,31 +7,6 @@ import LoginForm from './src/components/LoginForm';
 
 
 class App extends Component {
-
-// initializeFirebase() {
-//   const firebase = require("firebase");
-
-//   // Initialize Firebase
-//   var config = {
-//       apiKey: 'AIzaSyDpC3MY8oU6rveSP6DNVyWAfYLPRJIuajU',
-//       authDomain: 'authenticationfirenative.firebaseapp.com',
-//       databaseURL: 'https://authenticationfirenative.firebaseio.com',
-//       projectId: 'authenticationfirenative',
-//       storageBucket: 'authenticationfirenative.appspot.com',
-//       messagingSenderId: '572832231141'
-//   };
-//   firebase.initializeApp(config);
-
-//   //inicializando o firestore
-//   const firestore = require("firebase/firestore");
-//   db = firebase.firestore();
-//   db.settings({ timestampsInSnapshots: true });
-// }
-
-// componentWillMount() {
-//   this.initializeFirebase();
-// }
-  
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyDpC3MY8oU6rveSP6DNVyWAfYLPRJIuajU',
